83fcb77351599e720feeecf6565ec20e0402e98c
raveology0m <111902831+raveology0m@users.noreply.github.com>
2022-09-01 12:00:11 +0700

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: 'Tunic',
+    productNumber: '45626',
+    paymentStatus: 'Due',
+    shipping: 'Pending',
+  },
+  {
+    productName: 'Dress',
+    productNumber: '79656',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered',
+  },
+  {
+    productName: 'Abaya',
+    productNumber: '5226',
+    paymentStatus: 'Refunded',
+    shipping: 'Decline',
+  },
+  //   {
+  //     productName: 'Foldable mini Drone',
+  //     productNumber: '45626',
+  //     paymentStatus: 'Due',
+  //     shipping: 'Pending',
+  //   },
+  //   {
+  //     productName: 'Foldable mini Drone',
+  //     productNumber: '45626',
+  //     paymentStatus: 'Due',
+  //     shipping: 'Pending',
+  //   },
+  //   {
+  //     productName: 'Foldable mini Drone',
+  //     productNumber: '45626',
+  //     paymentStatus: 'Due',
+  //     shipping: 'Pending',
+  //   },
+];
